142fc1e9b9aaf9559dab16ca1d20268a92115daf
dltoth <119793952+dltoth@users.noreply.github.com>
2023-03-02 14:42:32 -0500

    Add files via upload

--- src/WebContext.cpp
+++ src/WebContext.cpp
@@ -1,9 +1,29 @@
+/**
+ * 
+ *  CommonUtil Library
+ *  Copyright (C) 2023  Daniel L Toth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published 
+ *  by the Free Software Foundation, either version 3 of the License, or any 
+ *  later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  The author can be contacted at dan@leelanausoftware.com  
+ *
+ */
 
 /** Class to Wrap minimal Web Server functionality necessary to implement 
  *  UPnPDevices. This is not all-inclusive but adaquate and should be implementable
  *  for a variety of Web Servers. 
  */
-
 #include "WebContext.h"
 
 /** Leelanau Software Company namespace 
--- src/WebContext.h
+++ src/WebContext.h
@@ -1,6 +1,27 @@
+/**
+ * 
+ *  CommonUtil Library
+ *  Copyright (C) 2023  Daniel L Toth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published 
+ *  by the Free Software Foundation, either version 3 of the License, or any 
+ *  later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  The author can be contacted at dan@leelanausoftware.com  
+ *
+ */
 
 /** Class to Wrap minimal Web Server functionality necessary to implement 
- *  UPnPDevices. WebContext will wrap ESP8266WebServer and ESP32 WebServer. 
+ *  UPnPDevices. WebContext will wrap ESP8266WebServer and (ESP32) WebServer. 
  *  Any other web server implementation should implement a subclass.
  *  
  *  This is not all-inclusive but adaquate for UPnPDevices and should be implementable
