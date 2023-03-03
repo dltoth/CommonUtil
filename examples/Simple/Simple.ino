376594b7aa0e61c376a02fc93deadad5e9358a11
dltoth <119793952+dltoth@users.noreply.github.com>
2023-03-03 09:43:55 -0500

    Add files via upload

--- examples/Simple/Simple.ino
+++ examples/Simple/Simple.ino
@@ -1,4 +1,26 @@
 /**
+ * 
+ *  CommonUtil Library
+ *  Copyright (C) 2023  Daniel L Toth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published 
+ *  by the Free Software Foundation, either version 3 of the License, or any 
+ *  later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *  The author can be contacted at dan@leelanausoftware.com  
+ *
+ */
+
+/**
  *   Simple example demonstrating the use of WebContext and CommonProgmem
  *   Input your ssid and psk on lines 10 and 11 of this application, then start the application with a Serial Monitor active and grab the
  *   IP address and port of the active Web Server
